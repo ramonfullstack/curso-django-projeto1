aa2b2a5a309e9ba8b303e8c96c3576e3aade7457
Ramon Santos <ramonss.bque@gmail.com>
2024-04-24 16:22:36 -0300

    Criado views e separado em arquivos

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,20 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
-
-def my_view(request):
-    return HttpResponse('UMA LINDA STRING')
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return HttpResponse('contato')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view),
-    path('sobre/', sobre), #/sobre/
-    path('contato/', contato), #/contato/
+    path('', include('recipes.urls')),
+    path('recipes/', include('recipes.urls')), #dominio com recipes
 ]
