ae03dd9da27b39cf34b1407e7af783bd548a70cb
Ramon Santos <ramonss.bque@gmail.com>
2024-04-24 00:19:43 -0300

    criado app recipes

--- projeto/urls.py
+++ projeto/urls.py
@@ -2,10 +2,19 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+
 def my_view(request):
     return HttpResponse('UMA LINDA STRING')
+
+def sobre(request):
+    return HttpResponse('sobre')
+
+def contato(request):
+    return HttpResponse('contato')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', my_view),
+    path('sobre/', sobre), #/sobre/
+    path('contato/', contato), #/contato/
 ]
