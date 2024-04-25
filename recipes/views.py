5cf63a444e499cbbd0340f7b39c9e0e318d47611
Ramon Santos <ramonss.bque@gmail.com>
2024-04-25 11:20:11 -0300

    Criado partials and page home

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
             'name': 'Ramon Santos',
         },
     status=200)
