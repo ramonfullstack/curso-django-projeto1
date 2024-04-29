a40e9bdbc9b0abaf1549d926dc45aa29537a7bb2
Ramon Santos <ramonss.bque@gmail.com>
2024-04-29 19:00:24 -0300

    criado mock para as telas

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-            'name': 'Ramon Santos',
+            'recipes': [make_recipe() for _ in range(10)],
         },
     status=200)
     
 # Create your views here.
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-            'name': 'Ramon Santos',
+            'recipe': make_recipe(),
         },
     status=200)
