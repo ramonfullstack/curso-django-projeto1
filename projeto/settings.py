9cb8e0080b7a1dbdf53ad6941abb97ad5a1ab7dd
Ramon Santos <ramonss.bque@gmail.com>
2024-05-02 12:43:38 -0300

    Registrado models no admin do django

--- projeto/settings.py
+++ projeto/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'base_static'
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
