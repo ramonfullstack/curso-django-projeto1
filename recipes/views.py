2e39b25c0c0083e7928f91f9fff1b785a7e94d69
Ramon Santos <ramonss.bque@gmail.com>
2024-05-03 11:17:58 -0300

    get_object_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
+from django.http import HttpResponse, Http404
 
 
 def home(request):
@@ -10,19 +11,33 @@ def home(request):
         },
     status=200)
     
+    
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+    
+    
     
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-    })
+        'title': f'{recipes[0].category.name} - Category | ',
+    }, status=200)
+    
     
 def recipe(request, id):
+    # recipe = Recipe.objects.filter(
+    #     pk=id,
+    #     is_published=True,
+    # ).order_by('-id').first()
+    
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+    
     return render(request, 'recipes/pages/category.html', context={
-            'recipe': make_recipe(),
+            'recipe': recipe,
             'is_detail_page': True,
         },
     status=200)
