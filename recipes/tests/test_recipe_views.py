987fdc4b1c7fef672dec90409f06a06d49a1a48d
Ramon Santos <ramonss.bque@gmail.com>
2024-05-05 19:11:29 -0300

    Criado teste de 404 no django

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -6,7 +6,11 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_views_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
-        
+    
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+    
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
@@ -23,6 +27,12 @@ class RecipeViewsTest(TestCase):
             reverse('recipes:category', kwargs={'category_id': 1})
         )
         self.assertIs(view.func, views.category)
+        
+    def test_recipe_category_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
 
     def test_recipe_detail_view_function_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
@@ -32,8 +42,13 @@ class RecipeViewsTest(TestCase):
         self.assertEqual(url, '/recipes/1/')
         #self.assertIs(view.func, views.recipe)
         
-    def test_recipe_home_view_returns_status_code_200_OK(self):
-        response = self.client.get(reverse('recipes:home'))
-        self.assertEqual(response.status_code, 200)
+    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1000})
+        response = self.client.get(
+            url
+        )
+        self.assertEqual(response.status_code, 404)
+        
+    
         
  
