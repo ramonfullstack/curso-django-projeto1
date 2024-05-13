182ad84f7d2ab6b40e4dc9617b3d86cfb6f1e061
Ramon Santos <ramonss.bque@gmail.com>
2024-05-13 14:54:06 -0300

    alterado constante perpage

--- recipes/views.py
+++ recipes/views.py
@@ -6,10 +6,12 @@ from django.db.models import Q
 from django.core.paginator import Paginator
 from utils.pagination import make_pagination, make_pagination_range
 
+PER_PAGE = 9
+
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('id')
     
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -25,7 +27,7 @@ def category(request, category_id):
         ).order_by('-id')
     )
     
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
@@ -58,7 +60,7 @@ def search(request):
         is_published=True
     ).order_by('-id')
     
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
