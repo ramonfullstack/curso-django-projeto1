eeaaa702be109b7f5e2e426e6fcc061e55ee2dc3
Ramon Santos <ramonss.bque@gmail.com>
2024-05-15 18:01:31 -0300

    widgets and configs the forms

--- authors/forms.py
+++ authors/forms.py
@@ -11,3 +11,32 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        
+        labels  = {
+            'username': 'username',
+            'first_name': 'first_name',
+            'last_name': 'last_name',
+            'email': 'email',
+            'password': 'password',
+        }
+        
+        help_texts = {
+            'Email': 'The e-mail be must valid',
+        }
+        
+        error_messages = {
+            'username' : {
+                'required': 'This field must not be empty',
+                'max_length': 'This field must have  less then 65 characters'
+            }
+        }
+        
+        widgets = {
+            'username': forms.TextInput(attrs=
+                        {
+                            'class': 'form-control', 'placeholder': 'Username'
+                        }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
