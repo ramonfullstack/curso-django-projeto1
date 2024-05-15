97ada99d20b79f4d382f1e2921eb4080b8c3266d
Ramon Santos <ramonss.bque@gmail.com>
2024-05-15 18:23:55 -0300

    Incluido erros de formularios

--- authors/forms.py
+++ authors/forms.py
@@ -1,7 +1,45 @@
 from django import forms
 from django.contrib.auth.models import User
 
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+    
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
+        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        )
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        })
+    )
+    
     class Meta:
         model = User
         fields = [
@@ -40,3 +78,45 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here'
             })
         }
+        
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atenção' in data:
+            raise forms.ValidationError(
+                'Não digite %(pipoca)s no campo password',
+                code='invalid',
+                params={'pipoca': '"atenção"'}
+            )
+
+        return data
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get('first_name')
+
+        if 'John Doe' in data:
+            raise forms.ValidationError(
+                'Não digite %(value)s no campo first name',
+                code='invalid',
+                params={'value': '"John Doe"'}
+            )
+
+        return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            password_confirmation_error = forms.ValidationError(
+                'Password and password2 must be equal',
+                code='invalid'
+            )
+            raise forms.ValidationError({
+                'password': password_confirmation_error,
+                'password2': [
+                    password_confirmation_error,
+                ],
+            })
--- recipes/views.py
+++ recipes/views.py
@@ -16,9 +16,9 @@ def home(request):
     
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
-    messages.error(request, 'Que legal foi um erro')
-    messages.success(request, 'Epa, você foi pesquisar algo que eu vi.')
-    messages.info(request, 'Epa, você foi pesquisar algo que eu vi.')
+    # messages.error(request, 'Que legal foi um erro')
+    # messages.success(request, 'Epa, você foi pesquisar algo que eu vi.')
+    # messages.info(request, 'Epa, você foi pesquisar algo que eu vi.')
     
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
