09989f7afa3affe04c7eecc66b3eff2a667a5629
Ramon Santos <ramonss.bque@gmail.com>
2024-05-16 16:54:22 -0300

    Terminado validação

--- authors/views.py
+++ authors/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import redirect, render
 from django.http import Http404
 from .forms import RegisterForm
+from django.contrib import messages
 
 def register_view(request):
-    request.session['number'] = request.session.get('number') or 1
-    request.session['number'] += 1
-    
-    
     register_form_data = request.session.get('register_form_data')
     form = RegisterForm(register_form_data)
         
@@ -22,8 +19,10 @@ def register_create(request):
     request.session['register_form_data'] = POST
     form = RegisterForm(request.POST)
     
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in')
+        
+        del(request.session['register_form_data'])
+        
     return redirect('authors:register')
-    
-    # return render(request, 'authors/pages/register_view.html', {
-    #     'form': form
-    # })
