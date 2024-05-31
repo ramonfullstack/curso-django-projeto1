4374fc6ae6b446256c18a34dfb26c5b0ca63151c
Ramon Santos <ramonss.bque@gmail.com>
2024-05-31 00:45:54 -0300

    Iniciado criação do dashboard

--- authors/views.py
+++ authors/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from authors.forms.login import LoginForm
+from recipes.models import Recipe
 from .forms import RegisterForm
 from django.contrib import messages
 
@@ -49,9 +50,7 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
 
-    print(form)
     if form.is_valid():
         authenticated_user = authenticate(
             username=form.cleaned_data.get('username', ''),
@@ -66,7 +65,7 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password')
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def logout_view(request):
@@ -81,3 +80,18 @@ def logout_view(request):
     messages.success(request, 'Logged out successfully')
     logout(request)
     return redirect(reverse('authors:login'))
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user
+    )
+    return render(
+        request,
+        'authors/pages/dashboard.html',
+        context={
+            'recipes': recipes,
+        }
+    )
+    
